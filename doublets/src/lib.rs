79398dbef7a10823cd7f46b3fe4e26defa090227
Stephen Wassell <stephen.wassell@sophos.com>
2020-11-01 17:12:35 +0000

    one thread

--- doublets/src/lib.rs
+++ doublets/src/lib.rs
@@ -1,3 +1,5 @@
+use crossbeam::scope;
+use crossbeam::crossbeam_channel::bounded;
 use fnv::FnvHashSet;
 
 // Things that don't change during the search and can be immutable.
@@ -14,9 +16,8 @@ struct State {
     word: Vec<char>,
     // The progress so far to the solution, starting with the head word.
     body: Vec<Vec<char>>,
-    // How many solutions were found.
-    // todo: change to Vec of solutions
-    count: usize,
+    // Solutions found on this thread: Vec of body Vecs.
+    solutions: Vec<Vec<Vec<char>>>,
     // The index of the last letter changed, so we don't change it again immediately.
     previous_i: usize,
 }
@@ -25,20 +26,22 @@ fn to_string(v: &[char]) -> String {
     v.iter().collect()
 }
 
-fn print_solution(f: &Fixed, s: &State) {
-    // The first word in the body vector is the original head.
-    // Print the head and tail in uppercase, the rest in lowercase.
-    print!("{} ", to_string(&s.body[0]).to_uppercase());
+fn print_solutions(fixed: &Fixed, solutions: &Vec<Vec<Vec<char>>>) {
+    for body in solutions {
+        // The first word in the body vector is the original head.
+        // Print the head and tail in uppercase, the rest in lowercase.
+        print!("{} ", to_string(&body[0]).to_uppercase());
 
-    for word in s.body.iter().skip(1) {
-        print!("{} ", to_string(&word));
+        for word in body.iter().skip(1) {
+            print!("{} ", to_string(&word));
+        }
+
+        println!(
+            "{} ({} steps)",
+            to_string(&fixed.tail).to_uppercase(),
+            body.len() - 1
+        );
     }
-
-    println!(
-        "{} ({} steps)",
-        to_string(&f.tail).to_uppercase(),
-        s.body.len() - 1
-    );
 }
 
 // Recursively search for a solution until we reach the specified depth.
@@ -60,8 +63,9 @@ fn find_rec(f: &Fixed, s: &mut State) {
 
             // Check if this is a solution before the dictionary check, in case the tail is not a real word.
             if s.word == f.tail {
-                print_solution(f, s);
-                s.count += 1;
+                // print_solution(f, s);
+                // let solution = s.body.clone();
+                s.solutions.push(s.body.clone());
             // Recurse if the word is not already used, and is in the dictionary.
             // Check the recursion limit first to avoid expensive lookups.
             } else if s.body.len() < f.depth
@@ -81,6 +85,38 @@ fn find_rec(f: &Fixed, s: &mut State) {
     s.body.pop();
 }
 
+fn start_threads(fixed: &Fixed, initial_state: State) -> Vec<Vec<Vec<char>>> {
+    let mut all_solutions = Vec::new();
+    let worker_count = 1; // todo: num of cores
+    
+    scope(|s| {
+        let (sender, receiver) = bounded(worker_count);
+        sender.send(initial_state).unwrap();
+        
+        let mut workers = Vec::new();
+        for _ in 0..worker_count {
+            let receiver_clone = receiver.clone();
+
+            workers.push(s.spawn(move |_| {
+                let mut solutions = Vec::new();
+
+                let mut state = receiver_clone.recv().unwrap();
+                find_rec(fixed, &mut state);
+                solutions.append(&mut state.solutions);
+
+                solutions
+            }))
+        }
+
+        for worker in workers {
+            let mut solutions = worker.join().unwrap();
+            all_solutions.append(&mut solutions);
+        }
+    }).unwrap();
+
+    all_solutions
+}
+
 pub fn find(head: &str, tail: &str, dict: FnvHashSet<String>, steps: usize) {
     // Things that don't change during the search.
     let fixed = Fixed {
@@ -90,24 +126,24 @@ pub fn find(head: &str, tail: &str, dict: FnvHashSet<String>, steps: usize) {
         depth: 1 + if steps == 0 { head.len() } else { steps },
     };
 
-    // The changing part of the state. It's updated during the recursion
+    // The changing part of the shared state. It's updated during the recursion
     // and only cloned to put in the queue for other threads.
-    let mut state = State {
+    let initial_state = State {
         word: head.to_lowercase().chars().collect(),
-        body: Vec::new(),
-        count: 0,
+        body: Vec::with_capacity(fixed.depth),
+        solutions: Vec::new(),
         // On the first call, previous_i must be outside the range of the string.
         previous_i: head.len(),
     };
 
-    state.body.reserve(fixed.depth);
-
     // Start the recursive search.
-    find_rec(&fixed, &mut state);
+    let solutions = start_threads(&fixed, initial_state);
+
+    print_solutions(&fixed, &solutions);
 
     println!(
         "Found {} solutions up to {} steps.",
-        state.count,
+        solutions.len(),
         fixed.depth - 1
     );
 }
